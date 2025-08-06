fe81619f39b8c3b5b5504672a1c161dcb8b19f27
MateusSantos007 <163445278+MateusSantos007@users.noreply.github.com>
2025-08-06 11:43:14 -0300

    Update exer5.dart

---
+++ Aula-02/exer5.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+Future<String> buscaPost() async {
+  var url = Uri.https('olinda.bcb.gov.br',
+'/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)',
+{'@dataCotacao': '\'08-04-2025\'',
+ '\$top':'100',
+  '\$format':'json',
+  '\$select':'cotacaoCompra'}
+    );
+
+  try {
+    var response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      var jsonBody = json.decode(response.body);
+
+      var cotacao = jsonBody['value'][0]['cotacaoCompra'].toString();
+      return cotacao;
+    } else {
+      return 'Erro ao acessar a API. Status Code: ${response.statusCode}';
+    }
+  } catch (e) {
+    return 'Ocorreu um erro na requisição: $e';
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: FutureBuilder<String>(
+            future: buscaPost(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return CircularProgressIndicator();
+              } else if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              } else {
+                return Text(snapshot.data != null ? 'Cotacao: ${snapshot.data}' : 'Vazio');
+              }
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
